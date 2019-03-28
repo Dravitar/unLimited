f47eb3b6cf855e077c8a386435184a224ee3e619
Alex Henderson <bohroco@gmail.com>
2019-03-28 06:45:27 +0100

    Update game.js

--- game.js
+++ game.js
@@ -57,12 +57,3 @@ function getProduction(time) {
     }
   }
 }
-
-function addElement(parentId, elementTag, elementId, html) {
-  // Adds an element to the document
-  var p = document.getElementById(parentId);
-  var newElement = document.createElement(elementTag);
-  newElement.setAttribute('id', elementId);
-  newElement.innerHTML = html;
-  p.appendChild(newElement);
-}
