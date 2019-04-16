769516b749450e5458c7e3dbd74b1371005d591a
Alex Henderson <bohroco@gmail.com>
2019-04-16 12:35:49 +0200

    Update mainTree.js

--- mainTree.js
+++ mainTree.js
@@ -1,2 +1,18 @@
 function treePress(index1, index2) {
+  let id=""+index1+index2;
+  if(player.clicks>0){
+    if(player.producers.power.gte(player.buttons.requirements.power[index1][index2])&&
+       player.buttons.unlocked.includes(player.buttons.requirements.buttons[index1][index2])){
+      docShow(id);
+      player.buttons.unlocked.push(id);
+    }
+  }
+}
+
+function docShow(id) {
+  document.getElementById(id).style.display = "";
+}
+
+function docHide(id) {
+  document.getElementById(id).style.display = "none";
 }
