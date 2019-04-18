6cecb11bca2e5a92296beef318d953bd497a84fc
Alex Henderson <bohroco@gmail.com>
2019-04-18 08:59:01 +0200

    Update mainTree.js

--- mainTree.js
+++ mainTree.js
@@ -1,11 +1,11 @@
 
 function treePress(index1, index2) {
-  if(player.clicks>0){
+  if(player.clicks>0&&!player.buttons.unlocked.includes(""+index1+index2)){
     if(player.producers.power.gte(player.buttons.requirements.power[index1][index2])&&
        player.buttons.unlocked.includes(player.buttons.requirements.buttons[index1][index2])){
       for(let i=0;i<player.buttons.unlocks[index1][index2].length;i++) {
-        let id="treeButton"+player.buttons.unlocks[index1][index2][i];
-        docShow(id);
+        let id=player.buttons.unlocks[index1][index2][i];
+        docShow("treeButton"+id);
         player.buttons.unlocked.push(id);
       }
       player.clicks--;
