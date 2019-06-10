7d5a0a8279b7e5e20da8cf30a944ac0afe5f1963
Alex Henderson <bohroco@gmail.com>
2019-06-10 16:57:11 +0200

    Update mainTree.js

--- mainTree.js
+++ mainTree.js
@@ -1,20 +1,19 @@
 
 function treePress(index1, index2) {
-  if(player.clicks>0&&!player.buttons.unlocked.includes(""+index1+index2)){
-    let r = "row"+index1;
-    let c = "col"+index2;
-    if(player.producers.power.gte(player.buttons.requirements[r][c][0])&&
-       player.buttons.unlocked.includes(player.buttons.requirements[r][c][1])){
-      console.log(player.buttons.requirements[r][c][0]);
-      player.producers.power = player.producers.power.minus(player.buttons.requirements[r][c][0]);
-      if(index1==3) buyProducer(index2);
-      for(let i=0;i<player.buttons.requirements[r][c][2].length;i++) {
-        let id=player.buttons.requirements[r][c][2][i];
-        docShow("treeButton"+id);
-        player.buttons.unlocked.push(player.buttons.requirements[r][c][3]);
-      }
-      player.clicks--;
+  let r = "row"+index1;
+  let c = "col"+index2;
+  if(player.clicks>0&&player.producers.power.gte(player.buttons.requirements[r][c][0])&&
+     player.buttons.unlocked.includes(player.buttons.requirements[r][c][1])){
+    player.producers.power = player.producers.power.minus(player.buttons.requirements[r][c][0]);
+    for(let i=0;i<player.buttons.requirements[r][c][2].length;i++) {
+      let id=player.buttons.requirements[r][c][2][i];
+      docShow("treeButton"+id);
     }
+    if(!player.buttons.unlocked.includes(""+index1+index2)) {
+      player.buttons.unlocked.push(player.buttons.requirements[r][c][3])
+    }
+    if(index1==3) buyProducer(index2);
+    player.clicks--;
   }
 }
 
