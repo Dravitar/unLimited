3042127caf6eb67e66e5df85f2de8b485bc26d9d
Alex Henderson <bohroco@gmail.com>
2020-04-07 10:56:11 -0400

    Update game.js

--- game.js
+++ game.js
@@ -12,7 +12,6 @@ function initializeGrid() {
         newSpan.style="display:grid;grid-column:"+i+"/"+ii+";grid-row:"+j+"/"+jj;
         newSpan.class="blank";
         newSpan.id="blank"+i+j;
-        newSpan.textContent=i+"/"+j;
         $('gameSpace').appendChild(newSpan);
       }
     }
