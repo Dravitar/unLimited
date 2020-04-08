92a095df135825885c3717eadd414985b7344e93
Alex Henderson <bohroco@gmail.com>
2020-04-08 09:28:48 -0400

    Update game.js

--- game.js
+++ game.js
@@ -86,17 +86,13 @@ function fadeOutAll(set) {
 function initializeGrid() {
   for(i=1;i<8;i++){
     for(j=1;j<8;j++){
-      if(i==4&&j==6){
-        continue;
-      } else {
-        let ii=i+1;
-        let jj=j+1;
-        var newSpan = document.createElement('span');
-        newSpan.style="display:grid;grid-column:"+i+"/"+ii+";grid-row:"+j+"/"+jj;
-        newSpan.class="blank";
-        newSpan.id="blank"+i+j;
-        $('gameSpace').appendChild(newSpan);
-      }
+      let ii=i+1;
+      let jj=j+1;
+      var newSpan = document.createElement('span');
+      newSpan.style="display:grid;grid-column:"+i+"/"+ii+";grid-row:"+j+"/"+jj;
+      newSpan.class="blank";
+      newSpan.id="blank"+i+j;
+      $('gameSpace').appendChild(newSpan);
     }
   }
 }
