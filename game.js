5984ec3e9fa1067787641be9dd564d796fe28c87
Alex Henderson <bohroco@gmail.com>
2020-04-15 20:20:56 -0400

    Update game.js

--- game.js
+++ game.js
@@ -172,6 +172,13 @@ function load() {
 			else if($(id).classList.contains("unlocked")) $(id).classList.remove("unlocked");
 			if(individualArray[2]) fadeIn(id);
 			else if($(id).style.opacity>0.5) fadeOut(id);
+			if($(id).classList.contains("quest")){
+				let index = parseInt(id.substring(id.length-1,id.length));
+				if(quests[index-1]){ 
+					$(id).classList.remove("unsolved");
+					$(id).classList.add("claimed");
+				}
+			}
 		}
 	}
 	//event.stopPropagation();
