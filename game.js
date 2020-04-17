661f11dc2920f1874a4bb311dafb4b6ad9bd4220
Alex Henderson <bohroco@gmail.com>
2020-04-17 09:18:23 -0400

    Update game.js

--- game.js
+++ game.js
@@ -178,7 +178,7 @@ function updateAll() {
 			$("quest5").classList.remove("unsolved");
 		}
 	}
-	if(player.upgrades.bankUnlock.amount.gt(0)){
+	if(player.upgrades.bankUnlock.purchased.gt(0)){
 		let num = player.upgrades.bankUnlock.amount;
 		for(i=1;i<5;i++){
 			if(i<num&&!$("clickBank"+i).classList.contains("unlocked")){
