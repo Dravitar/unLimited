c4baf77062f6a9b23faff16b8ca8fd1a089fcc3c
Alex Henderson <bohroco@gmail.com>
2020-04-21 15:36:12 -0400

    Update automation.js

--- automation.js
+++ automation.js
@@ -1,1 +1,39 @@
+function beginAutomationRecording() {
+	let good = true;
+	if(player.automationArray[0]!=null) if(!confirm("You have a saved automation. Do you wish to overwrite it?")) good = false;
+	if(good){
+		player.automationArray = [];
+		player.automating = true;
+		player.energy = new Decimal(0);
+		reset();
+		$("recordingSymbol").style.opacity = 1;
+		$("toggleAutomation").style.zIndex = 20;
+		$("toggleAutomation").style.opacity = 1;
+	}
+}
 
+function grabPiece(item) {	
+	let automationPiece = [];
+	let now = new Date().getTime();
+	let difference = now - player.lastAutomationAction;
+	player.lastAutomationAction = now;
+	automationPiece.push(difference);
+	automationPiece.push(item);
+	console.log(automationPiece);
+	player.automationArray.push(automationPiece);
+}
+
+function playAutomation(i) {
+	setTimeout( function() {
+		new Function(player.automationArray[i][0]));
+		var j = i+1;
+		if(player.automationArray[j]!=null) playAutomation(j);
+	}, player.automationArray[i][1]);
+}
+
+function stopAutomation() {
+	player.automating = false;
+	$("recordingSymbol").style.opacity = 0;
+	$("toggleAutomation").style.zIndex = 1;
+	$("toggleAutomation").style.opacity = 0;
+}
