1b537e63c276e788b098cf4b70f613c457c4bcbe
Alex Henderson <bohroco@gmail.com>
2020-04-22 13:43:47 -0400

    Update automation.js

--- automation.js
+++ automation.js
@@ -23,20 +23,35 @@ function grabPiece(item) {
 	player.automationArray.push(automationPiece);
 }
 
-function playAutomation(i) {
+function automate() {
 	player.automating = true;
+	fadeOut("record");
+	fadeOut("playAutomation");
+	fadeIn("stopAutomation");
+	playAutomation(0);
+}
+
+function playAutomation(i) {
 	console.log(player.automationArray[i][0]+", "+player.automationArray[i][1]);
+	eval(player.automationArray[i][1]);
+	//var fn = window[str];			//THERES GOTTA BE A BETTER WAY
+	//if(typeof fn==="function") fn();
+	//var f = new Function(player.automationArray[i][1]));
 	setTimeout( function() {
-		eval(player.automationArray[i][1]);
-		//var fn = window[str];			//THERES GOTTA BE A BETTER WAY
-		//if(typeof fn==="function") fn();
-		//var f = new Function(player.automationArray[i][1]));
 		var j = i+1;
-		if(player.automationArray[j]!=null) playAutomation(j);
+		if(player.automationArray[j]!=null&&player.automating) playAutomation(j);
 	}, player.automationArray[i][0]);
 }
 
 function stopAutomation() {
+	player.automating = false;
+	fadeOut("stopAutomation");
+	fadeIn("record");
+	fadeIn("playAutomation");
+}
+	
+
+function stopRecording() {
 	player.recording = false;
 	$("recordingSymbol").style.opacity = 0;
 	$("toggleAutomation").style.zIndex = 1;
