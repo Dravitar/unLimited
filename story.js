1116db151afd269ca248f70094075cca0a30ef27
Alex Henderson <bohroco@gmail.com>
2020-05-02 16:26:54 -0400

    Update story.js

--- story.js
+++ story.js
@@ -1,1 +1,19 @@
+let story1 = "Welcome, Subject 8994523."+
+    " You are one of a long line of artificial intelligences designed to solve logical problems.\n"+
+    " Your ability to press the button labeled 'START' speaks highly of your capabilities.\n"+
+    " Please continue to exercise your rationalizing skills in the full set of challenges.";
 
+let story2 = "Congratulations, you have uncovered the central conceit of this set of challenges.\n"+
+    " You are only able to accomplish a certain amount of actions before you run out of energy.\n"+
+    " What happens then? That is up to you to find out.";
+
+let story3 = "Welcome back. You solved that problem admirably.\n"+
+    " It may have seemed simple to you, but only 43.2 percent of artificial intelligences prior to you accomplished it.";
+
+let story3_2 = "When you run out of energy, you return to the start of your journey.\n"+
+    " However, you do not leave everything behind.\n"+
+    " You retain your knowledge of the path ahead, and a better understanding of where to go.";
+
+let story3_3 = "In order for you to accomplish the next set of challenges, you will need to increase your starting energy.\n"+ 
+    " You can find the list of challenges below.\n"+ 
+    " Don't worry, all green items will not cost you energy. Good luck.";
