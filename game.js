559e3694cf5108d5e3f18bd161dffe1805f39ea1
Alex Henderson <bohroco@gmail.com>
2020-05-02 20:19:01 -0400

    Update game.js

--- game.js
+++ game.js
@@ -233,6 +233,7 @@ function upgrade(item) { //Purchase an upgrade for Crystals
 			if(player.upgrades.bankUnlock.purchased.gt(i-1)) $("energyBanked"+i).style.display = "inline-block";
 			else $("energyBanked"+i).style.display = "none";
 		}
+		if(player.upgrades.bankUnlock.purchased.gt(0)) addClass("energyBanked","unlocked");
 		if(player.upgrades.bankUnlock.purchased.equals(4)&&!$("bankPowerupUpgrade").classList.contains("unlocked")){
 			$("crystalPowerupUpgrade").classList.add("unlocked");
 			fadeIn("crystalPowerupUpgrade");
